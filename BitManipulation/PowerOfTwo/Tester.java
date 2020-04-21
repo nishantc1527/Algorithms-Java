1aad06ed4381714e885694aeb33b2517e990fa4a
nishantc1527 <52992695+nishantc1527@users.noreply.github.com>
2020-04-20 17:36:32 -0700

    Added Power Of Two Problem And Solution

---
+++ BitManipulation/PowerOfTwo/Tester.java
@@ -0,0 +1,10 @@
+package BitManipulation.PowerOfTwo;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        System.out.println(PowerOfTwo.isPowerOfTwo(16));
+        System.out.println(PowerOfTwo.isPowerOfTwo(7));
+    }
+
+}
