6f701be667467fed254b920ff49157aa6bf447d4
nishantc1527 <52992695+nishantc1527@users.noreply.github.com>
2020-04-22 13:40:23 -0700

    Added Javadoc To Vertex

--- Algorithms/GraphTheory/Implementations/Vertex.java
+++ Algorithms/GraphTheory/Implementations/Vertex.java
@@ -4,9 +4,38 @@ import java.util.Objects;
 
 public class Vertex<E> {
 
+    /**
+     * The value of this vertex.
+     */
+
     public E val;
+
+    /**
+     * Used in shortest path algorithms. When the
+     * shortest path algorithm is finished, the prev
+     * value will point to the previous value in the
+     * shortest path.
+     */
+
     public Vertex<E> prev;
+
+    /**
+     * Used in shortest path algorithms. When the
+     * shortest path algorithm is finished, the dist
+     * value will be the total weight of the shortest
+     * path.
+     */
+
     public int dist;
+
+    /**
+     * Used in graph traversals. White means this
+     * vertex is not visited, grey means it is
+     * visited but it didn't visit all it's neighbors
+     * yet, and black means it's visited and all of it's
+     * neighbors is visited.
+     */
+
     public Color color;
 
     public Vertex(E val) {
