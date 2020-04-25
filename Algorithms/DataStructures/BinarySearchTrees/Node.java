ee9c9a12d598a2b779541d2af496c9dafb04dac2
nishantc1527 <52992695+nishantc1527@users.noreply.github.com>
2020-04-25 13:17:54 -0700

    Added Javadoc To Node

--- Algorithms/DataStructures/BinarySearchTrees/Node.java
+++ Algorithms/DataStructures/BinarySearchTrees/Node.java
@@ -2,13 +2,68 @@ package Algorithms.DataStructures.BinarySearchTrees;
 
 public interface Node<E extends Comparable<E>> {
 
+    /**
+     * Gets the value of this node.
+     *
+     * @return The value of this node.
+     */
+
     E getVal();
+
+    /**
+     * Gets the left child of this node.
+     *
+     * @return The left child of this node.
+     */
+
     Node<E> getLeft();
+
+    /**
+     * Gets the right child of this node.
+     *
+     * @return The right child of this node.
+     */
+
     Node<E> getRight();
+
+    /**
+     * Gets the parent of this node.
+     *
+     * @return The parent of this node.
+     */
+
     Node<E> getParent();
+
+    /**
+     * Sets the left child of this node.
+     *
+     * @param newNode This node's new left child.
+     */
+
     void setLeft(Node<E> newNode);
+
+    /**
+     * Sets the right child of this node.
+     *
+     * @param newNode The node's new right child.
+     */
+
     void setRight(Node<E> newNode);
+
+    /**
+     * Sets the parent of this node.
+     *
+     * @param newNode The node's new parent.
+     */
+
     void setParent(Node<E> newNode);
+
+    /**
+     * Sets the value of this node.
+     *
+     * @param newVal This node's new value.
+     */
+
     void setVal(E newVal);
 
 }
