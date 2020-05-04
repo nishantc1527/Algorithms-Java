a8df199b42980027badc7f1fe18c8192bef69352
nikhilc1527 <nikhilc1527@gmail.com>
2020-05-03 21:09:43 -0700

    Added Binary Search Trees to time comparison

--- Algorithms/DataStructures/BinarySearchTrees/Test.java
+++ Algorithms/DataStructures/BinarySearchTrees/Test.java
@@ -1,22 +1,26 @@
 package Algorithms.DataStructures.BinarySearchTrees;
 
 import Algorithms.DataStructures.BinarySearchTrees.AVLTree.AVLTree;
+import Algorithms.DataStructures.BinarySearchTrees.BinarySearchTree.BinarySearchTree;
 import Algorithms.DataStructures.BinarySearchTrees.RedBlackTree.RedBlackTree;
 
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        BinaryTree<Integer> rbtree = new RedBlackTree<>(), avltree = new AVLTree<>();
-        List<Integer> rbTreeVals = new ArrayList<>(), avlTreeVals = new ArrayList<>();
-        int n = 500;
+        BinaryTree<Integer> rbtree = new RedBlackTree<>(), avltree = new AVLTree<>(),
+                binarySearchTree = new BinarySearchTree<>();
+        List<Integer> rbTreeVals = new ArrayList<>(),
+                avlTreeVals = new ArrayList<>(), bstVals = new ArrayList<>();
+        int n = 50000;
         for (int i = 0; i < n; i++) {
-            int newVal = (int) (Math.random() * 1000 - 500);
+            int newVal = (int) (Math.random() * n * 4 - n * 2);
             rbTreeVals.add(newVal);
             avlTreeVals.add(newVal);
+            bstVals.add(newVal);
         }
 
-        long rbTimeStart, rbTimeFinish, avlTimeStart, avlTimeFinish;
+        long rbTimeStart, rbTimeFinish, avlTimeStart, avlTimeFinish, bstTimeStart, bstTimeFinish;
 
         rbTimeStart = System.currentTimeMillis();
         for (Integer rbTreeVal : rbTreeVals) {
@@ -30,10 +34,18 @@ public class Test {
         }
         avlTimeFinish = System.currentTimeMillis();
 
+        bstTimeStart = System.currentTimeMillis();
+        for (Integer bstTreeVal : bstVals) {
+            binarySearchTree.insert(bstTreeVal);
+        }
+        bstTimeFinish = System.currentTimeMillis();
+
         System.out.println("Red Black Tree insertion time for " + n + " values: " +
                 ((rbTimeFinish - rbTimeStart)) + " milliseconds");
         System.out.println("AVL Tree insertion time for " + n + " values: " +
                 ((avlTimeFinish - avlTimeStart)) + " milliseconds");
+        System.out.println("Binary Search Tree insertion time for " + n + " values: " +
+                ((bstTimeFinish - bstTimeStart)) + " milliseconds");
 
         System.out.println("\n");
 
@@ -49,9 +61,17 @@ public class Test {
         }
         avlTimeFinish = System.currentTimeMillis();
 
+        bstTimeStart = System.currentTimeMillis();
+        for (int i = 0; i < n; i++) {
+            binarySearchTree.delete(bstVals.remove(0));
+        }
+        bstTimeFinish = System.currentTimeMillis();
+
         System.out.println("Red Black Tree deletion time for " + n + " values: " +
                 ((rbTimeFinish - rbTimeStart)) + " milliseconds");
         System.out.println("AVL Tree deletion time for " + n + " values: " +
                 ((avlTimeFinish - avlTimeStart)) + " milliseconds");
+        System.out.println("Binary Search Tree deletion time for " + n + " values: " +
+                ((bstTimeFinish - bstTimeStart)) + " milliseconds");
     }
 }
