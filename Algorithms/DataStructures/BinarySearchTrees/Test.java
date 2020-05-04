063e3d7f2b0a6ff25830c2175fcfbccc1bc94570
nikhilc1527 <nikhilc1527@gmail.com>
2020-05-03 20:40:04 -0700

    Added a tester to test the time taken for insertion for avl trees vs red black trees

---
+++ Algorithms/DataStructures/BinarySearchTrees/Test.java
@@ -0,0 +1,39 @@
+package Algorithms.DataStructures.BinarySearchTrees;
+
+import Algorithms.DataStructures.BinarySearchTrees.AVLTree.AVLTree;
+import Algorithms.DataStructures.BinarySearchTrees.RedBlackTree.RedBlackTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        BinaryTree<Integer> rbtree = new RedBlackTree<>(), avltree = new AVLTree<>();
+        List<Integer> rbTreeVals = new ArrayList<>(), avlTreeVals = new ArrayList<>();
+        int n = 5000000;
+        for (int i = 0; i < n; i++) {
+            int newVal = (int) (Math.random() * 1000 - 500);
+            rbTreeVals.add(newVal);
+            avlTreeVals.add(newVal);
+        }
+
+        long rbTimeStart, rbTimeFinish, avlTimeStart, avlTimeFinish;
+
+        rbTimeStart = System.currentTimeMillis();
+        for (Integer rbTreeVal : rbTreeVals) {
+            rbtree.insert(rbTreeVal);
+        }
+        rbTimeFinish = System.currentTimeMillis();
+
+        avlTimeStart = System.currentTimeMillis();
+        for (Integer avlTreeVal : avlTreeVals) {
+            rbtree.insert(avlTreeVal);
+        }
+        avlTimeFinish = System.currentTimeMillis();
+
+        System.out.println("Red Black Tree insertion time for " + n + " values: " +
+                ((rbTimeFinish - rbTimeStart)) + " milliseconds");
+        System.out.println("AVL Tree insertion time for " + n + " values: " +
+                ((avlTimeFinish - avlTimeStart)) + " milliseconds");
+    }
+}
