d66c258995d6066be0d78be658f33887feb0662c
nishantc1527 <52992695+nishantc1527@users.noreply.github.com>
2020-05-09 14:39:43 -0700

    Added Binary Search Tree Tests

---
+++ src/Test/java/com/nishant/algorithms/DataStructures/BinarySearchTrees/BinarySearchTreeTest.java
@@ -0,0 +1,51 @@
+package com.nishant.algorithms.DataStructures.BinarySearchTrees;
+
+import com.nishant.algorithms.DataStructures.BinarySearchTrees.BinarySearchTree.BinarySearchTree;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTreeTest {
+
+    private BinarySearchTree<Integer> tree;
+
+    @Before
+    public void setup() {
+        tree = new BinarySearchTree<>();
+    }
+
+    @Test
+    public void insertTest() {
+        assertFalse(tree.contains(5));
+        tree.insert(5);
+        assertTrue(tree.contains(5));
+
+        for(int i = 0; i < 100; i ++) {
+            tree.insert((int) (Math.random() * 100));
+            assertTrue(tree.isValid());
+        }
+    }
+
+    @Test
+    public void deleteTest() {
+        tree.insert(5);
+        assertTrue(tree.contains(5));
+        tree.delete(5);
+        assertFalse(tree.contains(5));
+
+        int[] values = new int[100];
+
+        for(int i = 0; i < values.length; i ++) {
+            values[i] = (int) (Math.random() * 100);
+            tree.insert(values[i]);
+        }
+
+        for(int i : values) {
+            tree.delete(i);
+            assertTrue(tree.isValid());
+        }
+    }
+
+}
---
+++ src/Test/java/com/nishant/algorithms/DataStructures/BinarySearchTrees/RedBlackTreeTest.java
@@ -0,0 +1,52 @@
+package com.nishant.algorithms.DataStructures.BinarySearchTrees;
+
+import com.nishant.algorithms.DataStructures.BinarySearchTrees.AVLTree.AVLTree;
+import com.nishant.algorithms.DataStructures.BinarySearchTrees.RedBlackTree.RedBlackTree;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RedBlackTreeTest {
+
+    private RedBlackTree<Integer> tree;
+
+    @Before
+    public void setup() {
+        tree = new RedBlackTree<>();
+    }
+
+    @Test
+    public void insertTest() {
+        assertFalse(tree.contains(5));
+        tree.insert(5);
+        assertTrue(tree.contains(5));
+
+        for(int i = 0; i < 100; i ++) {
+            tree.insert((int) (Math.random() * 100));
+            assertTrue(tree.isValid());
+        }
+    }
+
+    @Test
+    public void deleteTest() {
+        tree.insert(5);
+        assertTrue(tree.contains(5));
+        tree.delete(5);
+        assertFalse(tree.contains(5));
+
+        int[] values = new int[100];
+
+        for(int i = 0; i < values.length; i ++) {
+            values[i] = (int) (Math.random() * 100);
+            tree.insert(values[i]);
+        }
+
+        for(int i : values) {
+            tree.delete(i);
+            assertTrue(tree.isValid());
+        }
+    }
+
+}
