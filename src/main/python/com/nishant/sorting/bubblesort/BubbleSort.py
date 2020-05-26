91da9fab8a25edbf7d1f71b656f3f63bbaf24442
Nishant Chatterjee <nishantc1527@gmail.com>
2020-05-25 18:47:24 -0700

    added python and bubble sort to python

---
+++ src/main/python/com/nishant/sorting/bubblesort/BubbleSort.py
@@ -0,0 +1,21 @@
+def bubble_sort(arr):
+    did_find = True
+    end = len(arr)
+
+    while end >= 0:
+        if did_find == False:
+            break
+
+        did_find = False
+
+        for i in range(end - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                did_find = True
+
+        end = end - 1
+
+arr = [1, 2, 6, 4, 5]
+print(str(arr))
+bubble_sort(arr)
+print(str(arr))
