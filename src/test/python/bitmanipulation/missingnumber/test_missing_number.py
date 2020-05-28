46aa43cd1dd1efd17f9d8c78dbab94d5ecc618ac
Nishant Chatterjee <nishantc1527@gmail.com>
2020-05-28 12:12:06 -0700

    made better test for missing number

--- src/test/python/bitmanipulation/missingnumber/test_missing_number.py
+++ src/test/python/bitmanipulation/missingnumber/test_missing_number.py
@@ -1,5 +1,16 @@
+import random
 from src.main.python.bitmanipulation.missingnumber.missing_number import missing_number
 
 
 def test_missing_number():
-    assert missing_number([0, 5, 3, 2, 4]) == 1
+    for i in range(2, 10000, 1):
+        rand = random.randrange(0, i + 1, 1)
+        new_list = []
+
+        for j in range(0, i + 1, 1):
+            if rand == j:
+                continue
+
+            new_list.append(j)
+
+        assert missing_number(new_list) == rand
