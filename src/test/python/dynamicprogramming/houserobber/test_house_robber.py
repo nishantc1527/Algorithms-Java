f1bfd260cf3069372f72060be68a53a4bdfe19da
Nishant Chatterjee <nishantc1527@gmail.com>
2020-05-30 12:41:50 -0700

    added dynamic programming

--- src/test/python/dynamicprogramming/houserobber/test_house_robber.py
+++ src/test/python/dynamicprogramming/houserobber/test_house_robber.py
@@ -1,4 +1,4 @@
-import random
+from random import randrange
 
 from src.main.python.dynamicprogramming.houserobber.house_robber import brute_force, memoized, bottom_up
 
@@ -12,18 +12,18 @@ def rob(nums):  # This function correctly works
 
 def test_brute_force():
     for i in range(100):
-        rand_list = [random.randrange(0, 1000, 1) for _ in range(25)]  # test size has to be small since brute
+        rand_list = [randrange(0, 1000, 1) for _ in range(25)]  # test size has to be small since brute
         # force is extremely slow
         assert rob(rand_list) == brute_force(rand_list)
 
 
 def test_memoized():
     for i in range(100):
-        rand_list = [random.randrange(0, 1000, 1) for _ in range(25)]
+        rand_list = [randrange(0, 1000, 1) for _ in range(25)]
         assert rob(rand_list) == memoized(rand_list)
 
 
 def test_bottom_up():
     for i in range(100):
-        rand_list = [random.randrange(0, 1000, 1) for _ in range(25)]
-        assert rob(rand_list) == bottom_up(rand_list), str(rand_list)
+        rand_list = [randrange(0, 1000, 1) for _ in range(25)]
+        assert rob(rand_list) == bottom_up(rand_list)
---
+++ src/test/python/dynamicprogramming/uniquepaths/test_unique_paths.py
@@ -0,0 +1,19 @@
+from math import factorial
+
+from src.main.python.dynamicprogramming.uniquepaths.unique_paths import brute_force, memoized
+
+
+def math_sol(m, n):  # the math solution; it is guaranteed to work
+    return factorial((m - 1) + (n - 1)) / (factorial(m - 1) * factorial(n - 1))
+
+
+def test_brute_force():
+    for i in range(1, 15, 1):
+        for j in range(1, 10, 1):
+            assert math_sol(i, j) == brute_force(i, j)
+
+
+def test_memoized():
+    for i in range(1, 20, 1):
+        for j in range(1, 20, 1):
+            assert math_sol(i, j) == memoized(i, j)
